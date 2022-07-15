5042c6122a2eb7a389950ef72efa204c5091e8c1
molumn <io.molumn@gmail.com>
2022-07-16 01:05:51 +0900

    :white_check_mark: try hashRouter again

--- profile-front/src/index.tsx
+++ profile-front/src/index.tsx
@@ -27,11 +27,11 @@ const root = ReactDOM.createRoot(document.getElementById('root')!!);
 
 root.render(
   <React.StrictMode>
-      <HashRouter basename={"https://molumn.github.io/molumn"}>
+      <HashRouter>
           <Routes>
               <Route index element={<Main />} />
-              <Route path={"/"} element={<Main />} />
-              <Route path={"/*"} element={<NotFound404 />}/>
+              <Route path={"/molumn/"} element={<Main />} />
+              <Route path={"/molumn/*"} element={<NotFound404 />}/>
           </Routes>
       </HashRouter>
   </React.StrictMode>
